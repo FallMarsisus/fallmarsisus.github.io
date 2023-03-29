7a98e63aa2ad0802eb138fe4b813f343288f539e
Marsisus <simonlncln@gmail.com>
2023-03-29 23:16:28 +0200

    test script py

---
+++ scripts/foo/bar/chap15ex1.py
@@ -0,0 +1,11 @@
+nom = 'Toto'
+print(nom, type(nom))
+
+age = 17
+print(age, type(age))
+
+occupations = ["Jeu Vidéos", "Lecture", "Ecouter de la musique"]
+print(occupations, type(occupations))
+
+dort = False
+print(dort, type(dort))
